f53ceabf58d69021a3b3035919cbff84c4922772
abbi-makarrall <102104555+abbi-makarrall@users.noreply.github.com>
2022-04-23 13:14:46 -0500

    Add files via upload

---
+++ Our Game/SceneLoader.cs
@@ -0,0 +1,22 @@
+// Written by Abbi MaKarrall
+// 4/13/2022
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit!");
+    }
+    
+}
