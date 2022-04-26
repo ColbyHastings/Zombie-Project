9f1fbe6056828d46b782cacc1cf3cbe8d2390684
ERC <joshua.vang@mnsu.edu>
2022-04-26 11:12:10 -0500

    first commit

---
+++ Our Game/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        HealthSystem healthSystem = new HealthSystem(100);
+
+        CMDebug.ButtonUI(new Vector2(100, 100), "damage" () => 
+        {
+            healthSystem.Damage(10);
+
+        }
+        Debug.Log("Health: " + healthSystem.GetHealth());
+        
+
+
+    }
+
+}
---
+++ Our Game/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,15 @@
+
+public class HealthSystem
+{
+    private int health;
+    public HealthSystem (int health)
+    {
+        this.health = health;
+    }
+
+    public int GetHealth()
+    {
+        return health;
+    }
+
+}
